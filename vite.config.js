5002cb09eb9ca81333962757886553123eff5514
David <davidolanrewaju538@gmail.com>
2024-11-09 14:18:14 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_ENV__: process.env.VITE_URL_API,
+  },
 });
